5d7d57a6741aa638e774cc4bd364c67dc1eb9ea9
animevn <haanhth21@gmail.com>
2020-03-13 09:08:45 +0700

    Initial Commit

---
+++ AnimatorIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatorIOS
+//
+//  Created by Anh Ha on 3/13/20.
+//  Copyright © 2020 Anh Ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
