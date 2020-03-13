70df5c6e579c98a346d9bd9c9417cd61a4bd709b
animevn <haanhth21@gmail.com>
2020-03-13 10:00:39 +0700

    change small layout

--- AnimatorIOS/Controller/AppDelegate.swift
+++ AnimatorIOS/Controller/AppDelegate.swift
@@ -8,28 +8,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)->Bool{
-        print("app did load")
+//        print("app did load")
         return true
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
-        print("app did become active")
+//        print("app did become active")
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-       print("app will become inactive")
+//       print("app will become inactive")
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
-        print("app did enter background")
+//        print("app did enter background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-       print("app will become active")
+//       print("app will become active")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        print("app did terminate")
+//        print("app did terminate")
     }
 }
 
--- AnimatorIOS/Controller/ViewController.swift
+++ AnimatorIOS/Controller/ViewController.swift
@@ -2,10 +2,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var cardImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        cardView.layer.borderColor = UIColor.clear.cgColor
+        cardView.layer.borderWidth = 0.5
+        cardView.layer.cornerRadius = 10
+        
     }
 
+    @IBAction func onRotatePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func onFlipLeftPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func onFadePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func onFlipRightPressed(_ sender: UIButton) {
+    }
+    
+    
 
 }
 
